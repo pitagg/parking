dc480217eae522ed6f12891a21fe7c2565599021
Pitágoras Gervasi Galiotto <pitagg@gmail.com>
2010-05-16 05:33:38 -0300

    Validações em Parking Spaces / Style in-line devido a problemas no rails

--- app/models/parking_space.rb
+++ app/models/parking_space.rb
@@ -1,5 +1,14 @@
 class ParkingSpace < ActiveRecord::Base
+  
+  #Referências
   belongs_to :park
   belongs_to :user
   belongs_to :vehicle_type
+  
+  # Validações
+  validates_presence_of :park_id
+  validates_presence_of :user_id
+  validates_presence_of :vehicle_type_id
+  validates_presence_of :amount
+  validates_presence_of :amount_free  
 end
